44451b97ec5f5dd932a5cc19a9b2ff901295d78a
mnn <>
2021-11-27 15:00:59 +0530

    Made it so that if the user inputs anything other than R, P or S, prints an Error message

--- rock_paper_scissors_1_4.py
+++ rock_paper_scissors_1_4.py
@@ -7,75 +7,49 @@
 
 import random
 
+# This gets the number based on the players choice
 def get_num(player):
     if player == 'R':
         plnum = 0
     elif player == 'P':
         plnum = 1
-    else:
+    elif player == 'S':
         plnum = 2
+    else:
+        plnum = -1
     return plnum
-        
-def dry_rps(player):
+
+# This gets the computer's choice to be compared with player's choice
+def get_comp():
     li = ['ROCK', 'PAPER', 'SCISSORS']
     comp_num = random.randint(0,2)
-
     comp_str = li[comp_num]
     print('The computer played:', comp_str)
+    return comp_num
 
+# Comparing the score of the player with that of the computer's.
+def play_rps(player):
     pl_num = get_num(player)
-    if (pl_num == 0 and comp_num == 2) or (comp_num == 0 and pl_num == 2):
-        if comp_num == 0:
-            print('The Computer won.')
-        else:
+    # If the user didn't choose an option, an error is printed.
+    if pl_num == -1:
+        print("Error. Pick from the abbreviations in the curly brackets")
+    else:
+        comp_num = get_comp()
+        if (pl_num == 0 and comp_num == 2) or (comp_num == 0 and pl_num == 2):
+            if comp_num == 0:
+                print('The Computer won.')
+            else:
+                print('You won!')
+        elif pl_num > comp_num:
             print('You won!')
-    elif pl_num > comp_num:
-        print('You won!')
-    elif pl_num < comp_num:
-        print('The Computer won.')
-    else:
-        print('Draw')
-    
-
-def rps(player):
-    li = ['ROCK', 'PAPER', 'SCISSORS']
-    ai = random.randint(0,2)
-    print(ai)
-
-    comp_str = li[ai]
-    print('The computer played:', comp_str)
-    ai += 1
-    if player =='R':
-        if ai == 1:
-            print('rock vs rock: draw')
-        if ai == 2:
-            print('rock vs paper: You win!')
-        if ai == 3:
-            print('rock vs scissor: Better luck next time!')
-    elif player =='P':
-        if ai == 1:
-            print('paper vs rock: You win!')
-        if ai == 2:
-            print('paper vs paper: draw')
-        if ai == 3:
-            print('paper vs scissor: Better luck next time!')
-
-    elif player =='S':
-        if ai == 1:
-            print('scissor vs rock: Better luck next time!')
-        if ai == 2:
-            print('scissor vs paper: You win!')
-        if ai == 3:
-            print('scissor vs scissor: draw')
-
-    else:
-        print("Pick from the abbreviations in the curly brackets.")
-
-    print('')
+        elif pl_num < comp_num:
+            print('The Computer won.')
+        elif pl_num == com_num:
+            print('Draw')
 
 
 
 ######### MAIN #########################
 while True:
     plr = input('{R}OCK, {P}APER, {S}CISSORS: ')
-    dry_rps(plr)
+    play_rps(plr)
